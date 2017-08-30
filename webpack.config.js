2b49a3e8055bfa59b9965f860a3f5608b2decb67
Giovanni Vinaccia <giovanni.vinaccia@justweb.it>
2017-08-30 17:22:28 +0200

    commit iniziale

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  devtool: isProd ? 'none' : 'inline-source-map',
+  entry: {
+    main: './index.ts',
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: '[name].bundle.js'
+  },
+  resolve: {
+    // Add `.ts` and `.tsx` as a resolvable extension.
+    extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
+  },
+  module: {
+    loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  },
+  context: path.join(__dirname, "src"),
+  plugins: [ ]
+}
